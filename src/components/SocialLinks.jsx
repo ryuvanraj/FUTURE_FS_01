fa54e8f675c0392880d7f3fe859e9ac1ce824f2d
ryuvanraj <yuvan.r2005@gmail.com>
2025-06-29 18:27:45 +0530

    SocialLink section creation

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SocialLinks = () => (
+  <div className="social">
+    <a href="https://www.linkedin.com/in/yuvan-raj/" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-linkedin"></i>
+    </a>
+    <a href="https://github.com/ryuvanraj" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-github"></i>
+    </a>
+    <a href="https://codepen.io/ryuvanraj" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-codepen"></i>
+    </a>
+  </div>
+);
+
+export default SocialLinks;
