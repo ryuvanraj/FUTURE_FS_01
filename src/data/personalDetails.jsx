a9ddf671aae2775eddb6447c61c8bf15d37c48c2
ryuvanraj <yuvan.r2005@gmail.com>
2025-06-29 18:28:27 +0530

    Personal Details and project data

---
+++ src/data/personalDetails.jsx
@@ -0,0 +1,11 @@
+export default {
+  name: "Yuvan Raj",
+  title: "Full Stack developer & Aspiring Web3 developer",
+  description: "",
+  email: "yuvan.r2005@example.com",
+  phone: "+91 9840 765 027",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/yuvan-raj/",
+    github: "https://github.com/ryuvanraj"
+  }
+};
